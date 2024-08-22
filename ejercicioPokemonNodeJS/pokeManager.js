9be6732d5c46397bcb6aba9e9f6518c898d0ed31
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-08-22 10:28:42 -0400

    ola

--- ejercicioPokemonNodeJS/pokeManager.js
+++ ejercicioPokemonNodeJS/pokeManager.js
@@ -1,6 +1,83 @@
 const file = require("./moduleFS")
 
-async function readJson(){
+let data = []
+let arrayContador = []
 
+async function leerArchivoJSON(){
+
+    try {
+        const archivo = await file.readThisFile("./files/informacionPoke.json")
+        if(archivo != null && archivo.length > 0){
+            const info = JSON.parse(archivo)
+
+            data = info.datos
+        }
+
+        console.log(archivo["datos"])
+        
+    } catch (error) {
+        console.log(error)
+    }
 }
+
+async function filtrarInfo() {
+    try {
+        await leerArchivoJSON()
+        const filtroAgua = data.filter(p => p['type'].includes('water') )
+
+        console.log("Filtro agua: ", filtroAgua)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+async function filtrarNombresP() {
+    try {
+        await leerArchivoJSON()
+        const cantidad = data.filter(p => p.name[0] == 'p').length
+        console.log("Cantidad por P: ",cantidad);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function contarPorTipo() {
+    try {
+        await leerArchivoJSON()
+
+        let tipos = []
+        tipos = data.map(p => {
+            return p['type'].join(',')
+        }).join(',').split(',')
+
+        console.log(tipos)
+
+        const tiposUnicos = [...new Set(tipos)]
+        console.log(tiposUnicos)
+
+        let contador = 0;
+
+        for(type of tiposUnicos){
+            contador = data.filter(p => p['type'].includes(type)).length
+            arrayContador.push({"tipo": type, "cantidad": contador})
+        }
+        console.log("arrayContador: ", arrayContador)
+    } catch (error) {
+        
+    }
+}
+
+async function ordenarPokemonMayorAMenor() {
+    await contarPorTipo()
+
+    let arrayOrdenado = arrayContador.sort((a, b) => a.cantidad - b.cantidad)
+    console.log("Array ordenado: ", arrayOrdenado.reverse())
+}
+
+//leerArchivoJSON()
+//filtrarInfo()
+//filtrarNombresP()
+//contarPorTipo()
+ordenarPokemonMayorAMenor()
+
 
