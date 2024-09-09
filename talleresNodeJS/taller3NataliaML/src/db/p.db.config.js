7d7e74fcfea9a639060d24dfe734cc7a90caa497
nataliamoraluq <mora.luq.natalia68@gmail.com>
2024-09-09 12:04:19 -0400

    Orobando taller3 laptop

--- talleresNodeJS/taller3NataliaML/src/db/p.db.config.js
+++ talleresNodeJS/taller3NataliaML/src/db/p.db.config.js
@@ -1,4 +1,7 @@
 // PRODUCT DB CONFIG --- IMPORT SEQUELIZE -- TO USE AND CONNECT WITH THE DB IN MYSQL
+//
+//
+// -----------------------------------
 import {Sequelize} from 'sequelize';
 export const sequelize = new Sequelize('products_express', 'root', 'GracoSoft#00', {
     dialect: 'mysql',
